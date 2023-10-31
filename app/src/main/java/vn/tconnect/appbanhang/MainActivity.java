669a6999e4d477f8ef7b87c7ceb4e4d524b5b699
kin203 <kien12342003@gmail.com>
2023-10-31 23:30:28 +0700

    initial

---
+++ app/src/main/java/vn/tconnect/appbanhang/MainActivity.java
@@ -0,0 +1,21 @@
+package vn.tconnect.appbanhang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    // khai báo các biến giao diện
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // ánh xạ id cho các biến giao diện
+
+    }
+}
---
+++ app/src/main/java/vn/tconnect/appbanhang/Slider.java
@@ -0,0 +1,68 @@
+package vn.tconnect.appbanhang;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class Slider extends AppCompatActivity {
+    ViewPager viewPager;
+    Button btnNext;
+    int[] layouts;
+    Adapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_slider);
+
+        viewPager=findViewById(R.id.pager);
+        btnNext =findViewById(R.id.btnNext);
+
+        layouts = new int[]{
+                R.layout.slide1,
+                R.layout.slide2,
+                R.layout.slide3
+        };
+        adapter = new Adapter(this,layouts);
+        viewPager.setAdapter(adapter);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (viewPager.getCurrentItem()+1< layouts.length){
+                    viewPager.setCurrentItem(viewPager.getCurrentItem()+1);
+                }else{
+                    //go to main activity
+
+                }
+            }
+        });
+        viewPager.addOnPageChangeListener(viewPagerChangeListener);
+    }
+    ViewPager.OnPageChangeListener viewPagerChangeListener= new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
+
+        }
+
+        @Override
+        public void onPageSelected(int i) {
+            if(i == layouts.length - 1){
+                btnNext.setText("Continue");
+            }else{
+                btnNext.setText("Next");
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+}
